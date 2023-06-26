7ee5157f2e2c7a6d678974ab623de861125a3eec
luckydn <107429613+luckydn@users.noreply.github.com>
2023-06-26 03:15:58 +0300

    Add files via upload

---
+++ hw2.sql
@@ -0,0 +1,51 @@
+USE hw2;
+CREATE TABLE sales 
+( id INT PRIMARY KEY AUTO_INCREMENT,
+	order_date DATE NOT NULL,
+	count_product INT NOT NULL
+);
+
+INSERT sales (order_date, count_product)
+VALUES
+	('2022-01-01', 156),
+	('2022-01-02', 180),
+    ('2022-01-03', 21),
+    ('2022-01-04', 124),
+    ('2022-01-05', 341);
+    
+SELECT * FROM sales;
+
+SELECT id AS "id заказа",
+IF(count_product < 100, "Маленький заказ",
+	IF(100 < count_product < 300 AND count_product > 300,"Средний заказ","Большой заказ"))
+    AS 'тип заказа'
+FROM sales;
+
+CREATE TABLE orders
+(
+id INT PRIMARY KEY AUTO_INCREMENT, 
+employee_id VARCHAR(15) NOT NULL,
+amount DOUBLE NOT NULL,
+order_status VARCHAR(12)
+);
+
+ALTER TABLE orders
+MODIFY COLUMN amount FLOAT;
+
+INSERT orders (employee_id, amount, order_status)
+VALUES
+	('e03', 15.00, 'OPEN'),
+    ('e01', 25.50, 'OPEN'),
+    ('e05', 100.70, 'CLOSED'),
+    ('e02', 22.18, 'OPEN'),
+    ('e04', 9.50, 'CANCELLED');
+    
+SELECT * FROM orders;
+SELECT id, employee_id, amount, order_status,
+	CASE
+		WHEN order_status = 'OPEN' THEN 'Order is in open state'
+        WHEN order_status = 'CLOSED' THEN 'Order is closed'
+        WHEN order_status = 'CANCELLED' THEN 'Order is cancelled'
+	END AS full_order_status
+FROM orders;
+
